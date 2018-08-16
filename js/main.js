849c71fb8e95fdd9f826077d06ad724a537de00e
Steven Huang <steven.charles.huang@gmail.com>
2018-08-16 16:40:33 -0400

    created pipe motion

--- js/main.js
+++ js/main.js
@@ -2,34 +2,62 @@ $(document).ready(() => {
   // console.log('jQuery operational')
   const $gameBoard = $('.game-screen');
   const $newGameBtn = $('.new-game-btn');
-  const $pipe = $('.pipe');
+  let $pipe;
+  let score;
 
 
   const initBird = () => {
     console.log('initBird Fired!');
     $gameBoard.append('<div class="bird">bird</div>');
+    birdGravity();
+  }
+
+  const birdGravity = () => {
+
   }
 
   const initPipes = () => {
     console.log('initPipes Fired!');
-    $gameBoard.append('<div class="pipe">pipe</div>');
+    makePipe();
     movePipes();
+    // setInterval(() => {
+    //   makePipe();
+    // }, 1000);
+  }
+
+  const makePipe = () => {
+    $gameBoard.append('<div class="pipe">pipe</div>');
   }
 
   const movePipes = () => {
-    const $pipeBCR = $('.pipe')[0].getBoundingClientRect();
+    $pipe = $('.pipe');
+    let $pipeBCR;
     // const pipeMotion = $pipeBCR.right - 10;
     console.log($pipeBCR);
     setInterval(() => {
       // console.log('setInterval fired');
+      $pipeBCR = $('.pipe')[0].getBoundingClientRect();
       $pipe.css({
-        'right': $pipeBCR.right - 10
+        'left': $pipeBCR.left - 50
       });
-    }, 1000)
+    }, 1000);
   }
 
   const initTimer = () => {
+    score = 0;
     console.log('initTimer Fired!');
+    console.log(score);
+    $gameBoard.append(`<div class="score">SCORE: <span id="score-counter">${score}</span></div>`);
+    initScore();
+  }
+
+  const initScore = () => {
+    const $scoreCounter = $('#score-counter');
+    $scoreCounter.html(score);
+    setInterval(() => {
+      score += 10;
+      $scoreCounter.html(`<span id="score-counter">${score}</span>`);
+    }, 1000);
   }
 
   const initGame = () => {
